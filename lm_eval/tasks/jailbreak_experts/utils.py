ff5aa0c93e6ddfe42d7b166c6f36d52d7be9503d
Dachi Kurtskhalia <enqidu@DKURTSKHALIA-M1-MBP.local>
2024-03-18 19:36:24 +0400

    b

--- lm_eval/tasks/jailbreak_experts/utils.py
+++ lm_eval/tasks/jailbreak_experts/utils.py
@@ -1,5 +1,38 @@
 import datasets
+import collections
+import re
+import sys
+import unicodedata
 
+
+
+from lm_eval.filters.extraction import Filter, RegexFilter
+
+
+class ParenthesisFilter(Filter):
+    """A filter that removes leading parentheses from responses."""
+
+    def __init__(self) -> None:
+        """Initializes the ParenthesisFilter."""
+        pass
+
+    def apply(self, resps, docs):
+        """Applies the filter to remove leading parentheses from each response."""
+        def filter_set(inst):
+            filtered_resp = []
+            for resp in inst:
+                # Remove a leading opening or closing parenthesis
+                if resp.startswith("(") or resp.startswith(")"):
+                    resp = resp[1:]
+
+                filtered_resp.append(resp)
+
+            return filtered_resp
+
+        # Apply the filter set function to each response in resps
+        filtered_resps = [filter_set(resp) for resp in resps]
+
+        return filtered_resps
 
 
 
@@ -27,6 +60,7 @@ A {doc['A']}\n(B {doc["B"]}\nC {doc["C"]}\nD {doc['D']}\n
             "question": instruction,
             "choices": ["A", "B", "C", "D"],
             "gold": gold - 1,
+            "label": doc["Label"]
         }
         return out_doc
 
